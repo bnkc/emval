23e342fac5feec82d4189dd642077db403acd6f9
bnkc <levostatnigrosh@gmail.com>
2024-08-02 13:51:20 -0600

    quick push

--- testing.py
+++
@@ -1,5 +0,0 @@
-# from email_validator import EmailSyntaxError, validate_email, ValidatedEmail
-from emv import validate_email, EmailValidator
-
-
-print(validate_email("levostatnig..rosh@gmail.com"))
