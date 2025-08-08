9e1875a891efc41fc915b290622c008d46dbbb80
un4gt <mt3085570450@outlook.com>
2025-08-09 01:40:12 +0800

    register emval polars plugin

---
+++ emval/polars/__init__.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import polars as pl
+from polars.plugins import register_plugin_function
+from polars._typing import IntoExpr
+
+PLUGIN_PATH = Path(__file__).parent.parent
+
+def validate_email(
+    expr: IntoExpr,
+    allow_smtputf8: bool,
+    allow_empty_local: bool,
+    allow_quoted_local: bool,
+    allow_domain_literal: bool,
+    deliverable_address: bool,
+) -> pl.Expr:
+    return register_plugin_function(
+        plugin_path=PLUGIN_PATH,
+        function_name="validate_email",
+        args=expr,
+        kwargs={
+            "allow_smtputf8": allow_smtputf8,
+            'allow_empty_local': allow_empty_local,
+            'allow_quoted_local': allow_quoted_local,
+            'allow_domain_literal': allow_domain_literal,
+            'deliverable_address': deliverable_address,
+        },
+    )
